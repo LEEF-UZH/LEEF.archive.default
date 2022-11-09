d0c9b24a84e06c57b2ebb2b9c3e975a3c1014388
Rainer M. Krug <Rainer@krugs.de>
2022-11-09 15:06:57 +0100

    Renamed to LEEF.2

--- R/register.R
+++ R/register.R
@@ -10,17 +10,17 @@
 #' @export
 #'
 register <- function(compression) {
-  if (is.null(system.file(package = "LEEF"))) {
+  if (is.null(system.file(package = "LEEF.2"))) {
     stop("This function requres the package to be installed!")
   }
 
   switch(
     compression,
-    "none"   = LEEF::add_archiver( run_archive_none ),
-    "tar"    = LEEF::add_archiver( run_archive_tar ),
-    "tar.gz" = LEEF::add_archiver( run_archive_tar.gz ),
-    "tar_subdir" = LEEF::add_archiver( run_archive_tar_subdir ),
-    "tar.gz_subdir" = LEEF::add_archiver( run_archive_tar.gz_subdir ),
+    "none"   = LEEF.2::add_archiver( run_archive_none ),
+    "tar"    = LEEF.2::add_archiver( run_archive_tar ),
+    "tar.gz" = LEEF.2::add_archiver( run_archive_tar.gz ),
+    "tar_subdir" = LEEF.2::add_archiver( run_archive_tar_subdir ),
+    "tar.gz_subdir" = LEEF.2::add_archiver( run_archive_tar.gz_subdir ),
     stop("not a valid compression!\n", "Allowed values are 'none', 'tar', und 'tar.gz.max'.")
   )
 
